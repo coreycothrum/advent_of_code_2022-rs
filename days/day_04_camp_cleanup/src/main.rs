14c8543047d434c142399f4a27b92ac526dfd972
Corey Cothrum <contact@coreycothrum.com>
2023-06-05 12:02:56 -0500

    chore(day_04): finish day 04 part 2

--- days/day_04_camp_cleanup/src/main.rs
+++ days/day_04_camp_cleanup/src/main.rs
@@ -5,7 +5,7 @@ use std::str::FromStr;
 fn main() {
     let input = fs::read_to_string("data/input.txt").unwrap();
 
-    let slackers = input.lines().fold(0, |acc, p| {
+    let (slackers, duplicates) = input.lines().fold((0, 0), |acc, p| {
         let mut p = p.split(',');
 
         let mut range1 = p.next().unwrap().split("-");
@@ -15,17 +15,29 @@ fn main() {
             <u64>::from_str(range1.next().unwrap()).unwrap()
                 ..=<u64>::from_str(range1.next().unwrap()).unwrap(),
         );
+
         let set2 = HashSet::<_>::from_iter(
             <u64>::from_str(range2.next().unwrap()).unwrap()
                 ..=<u64>::from_str(range2.next().unwrap()).unwrap(),
         );
 
-        acc + if set1.is_subset(&set2) || set1.is_superset(&set2) {
-            1_u64
-        } else {
-            0_u64
-        }
+        let slacker = acc.0
+            + if set1.is_subset(&set2) || set1.is_superset(&set2) {
+                1_u64
+            } else {
+                0_u64
+            };
+
+        let duplicate = acc.1
+            + if !set1.is_disjoint(&set2) {
+                1_u64
+            } else {
+                0_u64
+            };
+
+        (slacker, duplicate)
     });
 
     println!("elfs w/ nothing to do: {slackers}");
+    println!("pairs w/ that overlap: {duplicates}");
 }
