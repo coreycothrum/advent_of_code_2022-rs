b6dcf6d6170c7664993443b981e01506b08306c0
Corey Cothrum <contact@coreycothrum.com>
2023-06-05 09:59:20 -0500

    chore(day_03): finish day 03 part 2

--- days/day_03_rucksack_reorg/src/main.rs
+++ days/day_03_rucksack_reorg/src/main.rs
@@ -1,9 +1,19 @@
 use std::collections::HashSet;
 use std::fs;
 
+fn priority_mapping(c: &char) -> u8 {
+    // map to ascii, then scale
+    if c.is_lowercase() {
+        (*c as u8) - 96
+    } else {
+        (*c as u8) - 38
+    }
+}
+
 fn main() {
-    let sum: u64 = fs::read_to_string("data/input.txt")
-        .unwrap()
+    let input = fs::read_to_string("data/input.txt").unwrap();
+
+    let sum: u64 = input
         .lines()
         .map(|l| {
             let end: usize = l.len();
@@ -12,15 +22,28 @@ fn main() {
             let comp1 = HashSet::<_>::from_iter(l[0..half].chars());
             let comp2 = HashSet::<_>::from_iter(l[half..end].chars());
 
-            comp1.intersection(&comp2).fold(0, |acc, n| {
-                // get ascii value and scale
-                acc + (if n.is_lowercase() {
-                    (*n as u8) - 96
-                } else {
-                    (*n as u8) - 38
-                } as u64)
-            })
+            comp1
+                .intersection(&comp2)
+                .fold(0, |acc, c| acc + (priority_mapping(c) as u64))
         })
         .sum();
-    println!("{sum}");
+    println!("(part 1) priority sum of misplaced items: {sum}");
+
+    let sum: u64 = input
+        .lines()
+        .collect::<Vec<&str>>()
+        .chunks(3)
+        .map(|v| {
+            let comp1 = HashSet::<_>::from_iter(v[0].chars());
+            let comp2 = HashSet::<_>::from_iter(v[1].chars());
+            let comp3 = HashSet::<_>::from_iter(v[2].chars());
+            comp1
+                .intersection(&comp2)
+                .map(|x| *x)
+                .collect::<HashSet<_>>()
+                .intersection(&comp3)
+                .fold(0, |acc, x| acc + (priority_mapping(x) as u64))
+        })
+        .sum();
+    println!("(part 2) priority sum of badges: {sum}");
 }
