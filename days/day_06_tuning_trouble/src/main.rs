8190d824c08366efaa14ef97c0e00ad86fc5cd11
Corey Cothrum <contact@coreycothrum.com>
2023-06-27 07:54:42 -0500

    chore(day_06): day 06 part 2

--- days/day_06_tuning_trouble/src/main.rs
+++ days/day_06_tuning_trouble/src/main.rs
@@ -1,7 +1,7 @@
 use std::collections::HashSet;
 use std::fs;
 
-fn find_read_index_with_X_unique_chars(input: String, unique_count: usize) -> Option<usize> {
+fn find_read_index_with_X_unique_chars(input: &String, unique_count: usize) -> Option<usize> {
     for (idx, window) in input.as_bytes().windows(unique_count).enumerate() {
         let mut unique = HashSet::<u8>::from_iter(window.iter().cloned());
 
@@ -9,12 +9,15 @@ fn find_read_index_with_X_unique_chars(input: String, unique_count: usize) -> Op
             return Some(idx + unique_count);
         }
     }
-    return None
+    return None;
 }
 
 fn main() {
     let input = fs::read_to_string("data/input.txt").unwrap();
 
-    let part1 = find_read_index_with_X_unique_chars(input, 4).unwrap();
+    let part1 = find_read_index_with_X_unique_chars(&input, 4).unwrap();
     println!("{}", part1);
+
+    let part2 = find_read_index_with_X_unique_chars(&input, 14).unwrap();
+    println!("{}", part2);
 }
