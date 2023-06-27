78d2d9402253d79309b0ebe803b42e6a8b905e93
Corey Cothrum <contact@coreycothrum.com>
2023-06-27 07:53:12 -0500

    chore(day_06): pull logic into separate function

--- days/day_06_tuning_trouble/src/main.rs
+++ days/day_06_tuning_trouble/src/main.rs
@@ -1,16 +1,20 @@
 use std::collections::HashSet;
 use std::fs;
 
+fn find_read_index_with_X_unique_chars(input: String, unique_count: usize) -> Option<usize> {
+    for (idx, window) in input.as_bytes().windows(unique_count).enumerate() {
+        let mut unique = HashSet::<u8>::from_iter(window.iter().cloned());
+
+        if unique.len() == unique_count {
+            return Some(idx + unique_count);
+        }
+    }
+    return None
+}
+
 fn main() {
     let input = fs::read_to_string("data/input.txt").unwrap();
 
-    for (idx, window) in input.as_bytes().windows(4).enumerate() {
-        let mut unique = HashSet::<u8>::from_iter(window.iter().cloned());
-
-        if unique.len() == 4 {
-            println!("{:?}", unique);
-            println!("{}", idx + 4);
-            break;
-        }
-    }
+    let part1 = find_read_index_with_X_unique_chars(input, 4).unwrap();
+    println!("{}", part1);
 }
